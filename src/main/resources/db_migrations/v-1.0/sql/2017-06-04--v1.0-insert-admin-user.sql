e43af106e89ea997d7695db2fa2e9c88d33b37bb
Danila Vereshchakov <dvereschakov@gmail.com>
2017-06-18 16:17:51 +0300

    removed unnecessary sql script from changelog

--- src/main/resources/db_migrations/v-1.0/sql/2017-06-04--v1.0-insert-admin-user.sql
+++ src/main/resources/db_migrations/v-1.0/sql/2017-06-04--v1.0-insert-admin-user.sql
@@ -2,13 +2,13 @@ CREATE SEQUENCE IF NOT EXISTS public.account_generator
 INCREMENT 1
 MINVALUE 1
 MAXVALUE 9223372036854775807
-START 2
+START 1
 CACHE 1;
 ALTER TABLE public.account_generator
 OWNER TO postgres;
 
 INSERT INTO account (id, email, firstname, lastname, password, username, roles)
-  VALUES (2, 'dvereschakov@gmail.com', 'Данила', 'Верещаков', '$2a$10$f3tgC8zcEMZYX0U0wgl71u0wdbh34xMT3Zq39WALoDFTsAXNDgtSy', 'danila', 'ADMIN');
+  VALUES (1, 'dvereschakov@gmail.com', 'Данила', 'Верещаков', '$2a$10$f3tgC8zcEMZYX0U0wgl71u0wdbh34xMT3Zq39WALoDFTsAXNDgtSy', 'danila', 'ADMIN');
 
 
-INSERT INTO ROLES (account_id, roles) VALUES (2, 'ADMIN')
+INSERT INTO ROLES (account_id, roles) VALUES (1, 'ADMIN')
